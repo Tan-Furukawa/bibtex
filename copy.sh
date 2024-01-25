c38143cadedf3d675d9f068e304e36ad4a9c3677
Tan-Furukawa <furukawatan@gmail.com>
2024-01-25 20:51:40 +0900

    make copy

---
+++ copy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+destination_dir="$(dirname $0)"
+source_file="/../article/summary.bib"
+source_full_path="$destination_dir$source_file"
+
+echo "-----"
+echo $source_full_path
+echo $destination_dir
+echo "-----"
+
+# コピーを実行
+cp "$source_full_path" "$destination_dir"
+
+# 成功メッセージを表示
+echo "File $source_file が $destination_dir にコピーされました。"
