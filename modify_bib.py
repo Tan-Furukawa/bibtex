319c2e0bd9544b8b87147a9b395896f5a1253f8d
Tan-Furukawa <furukawatan@gmail.com>
2024-03-21 18:03:03 +0900

    update

---
+++ modify_bib.py
@@ -0,0 +1,31 @@
+
+#!----------------------------------------------------
+#! 注意! bibdeskと紐づいているパスでこのコードを実行しないこと
+#!----------------------------------------------------
+
+#%%
+import re
+def remove_url_fields_from_bib(bib_file, output_file):
+    with open(bib_file, 'r') as file:
+        bib_data = file.read()
+
+    # 正規表現を使って各エントリーの間を切り分ける
+    entries = re.split(r'(?=@)', bib_data)
+
+    # 各エントリーごとに処理
+    modified_entries = []
+    for entry in entries:
+        # 不要なフィールドを削除
+        entry = re.sub(r'(url|biburl|eprint)\s*=\s*{[^{}]*},?\n?', '', entry)
+        modified_entries.append(entry)
+
+    # 更新されたエントリーを結合
+    modified_bib_data = ''.join(modified_entries)
+
+    # 更新された内容を新しいファイルに書き込む
+    with open(output_file, 'w') as file:
+        file.write(modified_bib_data)
+
+# この部分は絶対に変更しない
+remove_url_fields_from_bib('summary.bib', 'summary_mod.bib')
+#%%
